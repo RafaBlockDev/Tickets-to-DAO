d30c0617c773c49445476f0ed6d799cdc36c33e0
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-10-12 21:34:04 -0500

    Upload

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const fs = require("fs");
 const { createCanvas, loadImage } = require("canvas");
+const console = require("console");
 const canvas = createCanvas(1000, 1000); //pixels
 const ctx = canvas.getContext("2d");
+
+const saveLayer = () => {
+    fs.writeFileSync("./newImage.png", )
+}
+
+
+const drawLayer = () => {
+    const image = await loadImage("./Coney - Blue#10");
+    ctx.drawImage(image, 0, 0, 1000, 1000);
+    console.log("This ran.");
+};
+
+drawLayer();
